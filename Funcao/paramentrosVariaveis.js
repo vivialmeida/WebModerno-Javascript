ede20efd8a6e1afea2d5be92a731a7d828bf9f6f
vivialmeida <viviane_k5@hotmail.com>
2019-09-26 23:17:59 -0300

    Adicionando tópico de Função

---
+++ Funcao/paramentrosVariaveis.js
@@ -0,0 +1,14 @@
+function soma(){
+    let soma =0
+    for (i in arguments){
+        soma += arguments[i]
+    }
+
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1,2,3,4))
+console.log(soma(1,2,3,4, "Teste"))
+console.log(soma('a', 'b', 'c')) 
---
+++ Funcao/parametrosPadrao.js
@@ -0,0 +1,36 @@
+//estrategia 1 para gerar valor padrão
+
+function soma1 (a, b,c){
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    return a + b + c
+}
+
+console.log( "Soma 1 : ", soma1(), soma1(3), soma1(1,2,3), soma1(0,0,0))
+
+
+// estrategia 2, 3 e 4 para gerar valores
+
+
+function soma2(a, b, c){
+     a = a !== undefined ? a: 1
+     b = 1 in arguments? b: 1
+     c = isNaN(c) ? 1 : c
+     return a+b+c
+}
+
+console.log( "Soma 2: " + soma2(), soma2(3), soma2(1,2,3), soma2(0,0,0))
+
+
+function soma3( a = 1, b = 1, c =1){
+    return a + b + c
+}
+
+console.log("Soma 3: " + soma3(), soma3(3), soma2(1,2,3), soma3(0,0,0))
+
+function mult ( a, b){
+    return a* b
+}
+
+console.log(mult(2,6))
